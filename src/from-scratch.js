562d5733fb355ce0dba1acb7b3dbb03e9d5e8527
Julioj1 <dbjulioj@gmail.com>
2025-11-03 19:52:35 -0500

    Completed makeIdFunc

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,10 @@
 const makeIdFunc = () => {
-
+  let count = 1;
+  const counter = () => {
+    console.log(count);
+    return count++;
+  }
+  return counter
 };
 
 const makePasswordChecker = (correctPassword) => {
